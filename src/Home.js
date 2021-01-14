5ce5e78b1c2b2ec4214d282b6f05c21dbf6fd054
schmidtb-de <benny@schmidtb.de>
2021-01-14 17:02:49 +0100

    initial

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import React from "react"
+import "./Home.css"
+import Product from "./Product"
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+          className="home__image"
+        />
+
+        <div className="home__row">
+          <Product
+            title="The lean startup"
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            price={29.99}
+            rating={5}
+          />
+          <Product />
+        </div>
+
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzaB10l2HXv0GDdrYWUMW5CIUQ-dkD8Sg",
+  authDomain: "clone-d7846.firebaseapp.com",
+  projectId: "clone-d7846",
+  storageBucket: "clone-d7846.appspot.com",
+  messagingSenderId: "211262817968",
+  appId: "1:211262817968:web:a49c141d4a600eb01f1d75",
+};
