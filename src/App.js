12f23f83e949d3edf5fab1f0648e3f3fce608d4e
schmidtb-de <benny@schmidtb.de>
2021-01-21 23:10:48 +0100

    sign in & sign out working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./App.css"
 import Header from "./Header"
 import Home from "./Home"
 import Checkout from "./Checkout"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from "./Login"
+import { auth } from "./firebase"
+import { useStateValue } from "./StateProvider"
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is: >", authUser)
+
+      if (authUser) {
+        // User logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      } else {
+        // User is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        })
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider"
 import Subtotal from "./Subtotal"
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue()
+  const [{ basket, user }, dispatch] = useStateValue()
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,6 +15,7 @@ function Checkout() {
           className="checkout__ad"
         />
         <div>
+          <h3>Hello, {user ? user.email : "Guest"}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
